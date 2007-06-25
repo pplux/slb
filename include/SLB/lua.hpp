9d7694c9c5c86c9257121782ad6ae828a6696265
pplux <pplux@localhost>
2007-06-25 10:59:44 +0000

    improved lua dependency, now we can use internal or external lua

--- include/SLB/lua.hpp
+++ include/SLB/lua.hpp
@@ -24,10 +24,16 @@
 #define __SLB_LUA__
 
 extern "C" {
-	// Local (static) lua (v 5.1.1)
+#ifdef EXTERNAL_LUA
+	#include <lua.h>
+	#include <lauxlib.h>
+	#include <lualib.h>
+#else
+	// Local (static) lua (v 5.1.2)
 	#include "lua/lua.h"
 	#include "lua/lauxlib.h"
 	#include "lua/lualib.h"
+#endif
 }
 
 #endif
