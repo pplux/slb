ec7951d4f060e1c04fcf83211c73e595372aa33e
pplux <pplux@pplux.com>
2010-12-30 15:13:30 +0100

    fixed compilation problems on windows

--- src/lua.c
+++ src/lua.c
@@ -1,4 +1,29 @@
-/* one.c -- Lua core, libraries, and interpreter in a single file
+/*
+ inspired by one.c -- Lua core, libraries, and interpreter in a single file
+
+    SLB - Simple Lua Binder
+    Copyright (C) 2007-2010 Jose L. Hidalgo Valiño (PpluX)
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+  
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+  
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+	
+	Jose L. Hidalgo (www.pplux.com)
+	pplux@pplux.com
 */
 
 #define MAKE_LIB  1
@@ -13,6 +38,7 @@
        #endif
 #endif
 
+#define lobject_c
 
 /* default is to build the full interpreter */
 #ifndef MAKE_LIB
@@ -32,7 +58,6 @@
 #define LUA_ANSI
 #endif
 
-#define luaall_c
 
 /* core -- used by all */
 #include "lua/lapi.c"
@@ -59,6 +84,8 @@
 #undef pushclosure
 
 /* auxiliary library -- used by all */
+
+
 #include "lua/lauxlib.c"
 
 /* standard library  -- not used by luac */
