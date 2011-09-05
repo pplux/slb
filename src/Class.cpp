c597d9dd4b7fa277a523442a5f9d18d711cb30bf
pplux <pplux@pplux.com>
2011-09-05 12:20:47 +0200

    updated license

--- src/Class.cpp
+++ src/Class.cpp
@@ -1,6 +1,6 @@
 /*
     SLB - Simple Lua Binder
-    Copyright (C) 2007-2010 Jose L. Hidalgo Valiño (PpluX)
+    Copyright (C) 2007-2011 Jose L. Hidalgo Valiño (PpluX)
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
